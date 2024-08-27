65562468067d0998e09443bc716f68e54d6a9480
Yılmaz Ölmez <yilmazlmez@gmail.com>
2024-08-27 22:11:37 +0300

    Migratory birds second way solve

---
+++ Migratory Birds/Solve2.cs
@@ -0,0 +1,29 @@
+﻿
+List<int> arr = new();
+
+
+var distinctArr = arr.Distinct().Order();
+
+Dictionary<int, int> dic = new();
+
+foreach (var item in distinctArr)
+{
+    var countArr = arr.Where(x => x == item).Count();
+
+    dic.Add(item, countArr);
+}
+
+int maxType = dic.First().Key;
+int maxTypeNumber = dic.First().Value;
+
+foreach (var item in dic)
+{
+    if (item.Value > maxTypeNumber)
+    {
+        maxType = item.Key;
+        maxTypeNumber = item.Value;
+    }
+}
+
+return maxType;
+        
